6a39ce50c50374385eac392319fb9b829bf31164
kcbobbe <kcbobbe@gmail.com>
2020-03-04 12:44:54 -0500

    The readme is populated with every required value now, but I think my implementation relies too heavily on the editor. There's probably ways to make it a lot better

--- README.md
+++ README.md
@@ -1,12 +1,38 @@
-# Katie's Master Work
-  ## By Katie Bobbe | null
-  ![Katie's Pic](https://avatars2.githubusercontent.com/u/38509741?v=4)
-  ## Description
 
-  
+# Katie's Awesome Project
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+## By Katie Bobbe | null
+![Katie's Pic](https://avatars2.githubusercontent.com/u/38509741?v=4)
 
-  ## Table Of Contents
-  1.
-  ## Installation
-  ## Usage
-  ## Credits 
+## Table Of Contents
+1. Description
+2. Technologies Used
+3. Installation
+4. Usage
+5. Contributors
+6. Questions
+7. Credits
+
+## Description
+This awesome project creates readme files from user input from the cli
+## Technologies Used
+* JavaScript
+    
+## Installation
+1. Clone the repository
+2. npm install
+3. node index.js
+
+## Usage
+1. answer the questions in the cli
+2. copy the readme to your project
+
+## Contributors
+1. Katie Bobbe
+
+## Questions
+1. I think my implementation of this project was too basic and I would like to find out how I can make it better.
+
+## Credits 
+1. mdn
+
--- index.js
+++ index.js
@@ -6,10 +6,12 @@ let username = "";
 let picture = "";
 let email = "";
 
+// for the editor questions, the default editor will be launched. For my machine, it is Vim.
+// press i to start typing, press esc key then :x enter to save your answer.
+
 axios
   .get("https://api.github.com/users/kcbobbe")
   .then(function(res) {
-    // console.log(res.data);
     username = res.data.name;
     picture = res.data.avatar_url;
     email = res.data.email;
@@ -22,7 +24,7 @@ inquirer.prompt([
     message: "What is your project's title?"
   },
   {
-    type: "editor",
+    type: "input",
     name: "description",
     message: "Write a brief description of your application."
   },
@@ -37,22 +39,95 @@ inquirer.prompt([
       "Python",
       "JavaScript"
     ]
+  },
+  {
+    type: "editor",
+    message: "What are the steps to install the application?",
+    name: "installation",
+  },
+  {
+    type: "editor",
+    message: "Describe the usage of the application.",
+    name: "usage",
+  },
+  {
+    type: "editor",
+    message: "Add credits",
+    name: "credits",
+  },
+  {
+    type: "editor",
+    message: "Add contributors",
+    name: "contributors",
+  },
+  {
+    type: "editor",
+    message: "Any questions?",
+    name: "questions",
+  },
+  {
+    type: "list",
+    message: "Which license would you like to use?",
+    default: "MIT",
+    name: "license",
+    choices: [
+      "MIT", 
+      "Apache 2.0",
+      "GNU GPL v3.0"
+    ]
   }
 ]).then(function(data) {
   console.log(data.title)
-  var readmeText = 
-  `# ${data.title}
-  ## By ${username} | ${email}
-  ![Katie's Pic](${picture})
-  ## Description
+  console.log(data.stack)
 
-  ${data.description}
+  let licenseBadge = "";
+  if (data.license === "MIT"){
+    licenseBadge = `\r[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  } else if (data.license === "Apache 2.0"){
+    licenseBadge = `\r[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  } else if (data.license === "GNU GPL v3.0"){
+    licenseBadge= `\r[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+  }
 
-  ## Table Of Contents
-  1.
-  ## Installation
-  ## Usage
-  ## Credits `
+  var stackList = "";
+  data.stack.forEach(stack => {
+    stackList = stackList + 
+    `\r* ${stack}
+    `
+    console.log(stackList)
+  })
+  
+  let readmeText = 
+`
+# ${data.title}
+${licenseBadge}
+## By ${username} | ${email}
+![Katie's Pic](${picture})
+
+## Table Of Contents
+1. Description
+2. Technologies Used
+3. Installation
+4. Usage
+5. Contributors
+6. Questions
+7. Credits
+
+## Description
+${data.description}
+## Technologies Used
+${stackList}
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## Contributors
+${data.contributors}
+## Questions
+${data.questions}
+## Credits 
+${data.credits}
+`
 
     
 
